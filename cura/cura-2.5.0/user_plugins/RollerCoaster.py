50d7fea90675674310bf63a5a4ff0c77cea1ef0f
Juergen Weigert <jw@owncloud.com>
2017-08-04 18:48:27 +0200

    Gui draft for RollerCoaster

--- cura/cura-2.5.0/user_plugins/RollerCoaster.py
+++ cura/cura-2.5.0/user_plugins/RollerCoaster.py
@@ -13,7 +13,8 @@ else:
       pass
 
     def getSettingValueByKey(self, name):
-      if name == "pause_height": return 4
+      """ code all parameters here, for standalone usage """
+      if name == "max_grade_perc": return 50
       return 0
 
     ##  Copied from Script.py:
@@ -33,64 +34,143 @@ else:
 
 
 class RollerCoaster(Script):
+    version = "0.1"
     def __init__(self):
         super().__init__()
 
     def getSettingDataString(self):
+        """
+           A json structure to be parsed by 
+           https://github.com/Ultimaker/Uranium/blob/2.5/UM/Settings/SettingFunction.py
+        """
+
         return """{
-            "name":"RollerCoaster layer height manipulation",
+            "name":"RollerCoaster """ + self.version + """ (layer height manipulation)",
             "key": "RollerCoaster",
             "metadata": {},
             "version": 2,
             "settings":
             {
-                "pause_height":
+                "max_grade_perc":
                 {
-                    "label": "RollerCoaster height",
-                    "description": "RollerCoaster At what height should the pause occur",
+                    "label": "Maximum grade",
+                    "description": "Percentage rise over run [0..100..], limited e.g. by nozzle geometry.",
+                    "unit": "%",
+                    "type": "float",
+                    "default_value": 50.0,
+                    "minimum_value": 10.0,
+                    "maximum_value_warning": 100.0
+                },
+                "mode":
+                {
+                    "label": "Linear or polar modes",
+                    "description": "Apply in X or Y orientation or radial or along the circumfere.",
+                    "type": "enum",
+                    "options": { "x": "X-axis (linear)", "y": "Y-axis (linear)", "c": "Circumfere (polar)", "r": "Radial (from center)" },
+                    "default_value": "x"
+                },
+                "periods":
+                {
+                    "label": "Nr. of periods",
+                    "description": "how often to swing back and forth in the horizontal.",
+                    "unit": "periods",
+                    "type": "float",
+                    "default_value": 0.5,
+                    "minimum_value": 0.1,
+                    "minimum_value_warning": 0.49,
+                    "maximum_value_warning": 100
+                },
+                "start_perc":
+                {
+                    "label": "Start offset percentage",
+                    "description": "0%: start low at minimum X/Y or at center (if radial). 100% starts high (at half a period)",
+                    "unit": "%",
+                    "type": "float",
+                    "default_value": 0,
+                    "minimum_value": 0,
+                    "maximum_value": 200
+                },
+                "max_layer_mul":
+                {
+                    "label": "Maxiumum multiplier for layer height",
+                    "description": "Factor for maximum enlargement of a single layer.",
+                    "unit": "layer heights",
+                    "type": "float",
+                    "default_value": 2.0,
+                    "minimum_value": 1.0,
+                    "maximum_value_warning": 4.0
+                },
+                "min_layer_mul":
+                {
+                    "label": "Minimum multiplier for layer height",
+                    "description": "Factor for smallest compression of a single layer.",
+                    "unit": "layer heights",
+                    "type": "float",
+                    "default_value": 0.5,
+                    "minimum_value": 0.1,
+                    "minimum_value_warning": 0.25,
+                    "maximum_value": 1.0
+                },
+                "use_bb":
+                {
+                    "label": "Use automatic bounding box",
+                    "description": "auto: determine maximum and minimum coordinates from the object bounding box; or manual: specify manually.",
+                    "type": "enum",
+                    "options": { "auto": "Automatic", "manual": "Manual" },
+                    "default_value": "auto"
+                },
+                "min_x":
+                {
+                    "label": "Minimum X Coordinate",
+                    "description": "X position where the first period begins. Everything beyond remains flat.",
                     "unit": "mm",
                     "type": "float",
-                    "default_value": 5.0
+                    "default_value": -30,
+                    "enabled": "use_bb == 'manual'"
                 },
-                "head_park_x":
+                "max_x":
                 {
-                    "label": "RollerCoaster Park print head X",
-                    "description": "RollerCoaster What x location does the head move to when pausing.",
+                    "label": "Maximum X Coordinate",
+                    "description": "X position where the last period ends. Everything beyond remains flat.",
                     "unit": "mm",
                     "type": "float",
-                    "default_value": 190
+                    "default_value": 30,
+                    "enabled": "use_bb == 'manual'"
                 },
-                "head_park_y":
+                "min_y":
                 {
-                    "label": "RollerCoaster Park print head Y",
-                    "description": "RollerCoaster What y location does the head move to when pausing.",
+                    "label": "Minimum Y Coordinate",
+                    "description": "Y position where the first period begins. Everything beyond remains flat.",
                     "unit": "mm",
                     "type": "float",
-                    "default_value": 190
+                    "default_value": -30,
+                    "enabled": "use_bb == 'manual'"
                 },
-                "retraction_amount":
+                "max_y":
                 {
-                    "label": "RollerCoaster Retraction",
-                    "description": "RollerCoaster How much fillament must be retracted at pause.",
+                    "label": "Maximum Y Coordinate",
+                    "description": "Y position where the last period ends. Everything beyond remains flat.",
                     "unit": "mm",
                     "type": "float",
-                    "default_value": 0
+                    "default_value": 30,
+                    "enabled": "use_bb == 'manual'"
                 },
-                "extrude_amount":
+                "adaptive_flow_perc":
                 {
-                    "label": "RollerCoaster Extrude amount",
-                    "description": "RollerCoaster How much filament should be extruded after pause. This is needed when doing a material change on Ultimaker2's to compensate for the retraction after the change. In that case 128+ is recommended.",
-                    "unit": "mm",
+                    "label": "Adapt flow rate",
+                    "description": "0%: do not adapt flow rate; 100%: compensate flow for thicker and thinner layers",
                     "type": "float",
-                    "default_value": 0
+                    "default_value": 100,
+                    "minimum_value": 0,
+                    "maximum_value_warning": 200
                 },
-                "redo_layers":
+                "shape":
                 {
-                    "label": "RollerCoaster Redo layers",
-                    "description": "RollerCoaster Redo a number of previous layers after a pause to increases adhesion.",
-                    "unit": "layers",
-                    "type": "int",
-                    "default_value": 0
+                    "label": "Pattern Shape",
+                    "description": "Repetitive shape of the pattern.",
+                    "type": "enum",
+                    "options": { "triangular": "Triangles", "sinuide": "Sinus", "concave": "Concave", "convex": "Convex" },
+                    "default_value": "triangular"
                 }
             }
         }"""
